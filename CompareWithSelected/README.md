6a9eb1bfd76e73d8c87cafebb48bf737e4ac55c8
sebahattin <sebkucuk05@gmail.com>
2023-01-31 23:47:13 +0300

    first commit

---
+++ CompareWithSelected/README.md
@@ -0,0 +1,10 @@
+# React ile Kameradan Barkod Okutma Uygulaması
+
+Derste birlikte hazırladığımız kaynak kodlar.
+
+Web kamerasından okutulan barkodları yakalıyor ve api'ye gönderip ilgili ürünün detaylarını döndürüyor.
+Tabi API kısmını size bırakıyorum derste yazdığım basit bir şey olduğu için onu eklemedim.
+
+**Ders Videosu**
+
+[https://www.youtube.com/watch?v=-F2u__EUQGM](https://www.youtube.com/watch?v=-F2u__EUQGM)
---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useRef, useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+
+  const video = useRef(null);
+  const canvas = useRef(null);
+  // const [barcode, setBarcode] = useState('');
+
+  const openCam = () => {
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        width: 1280,
+        height: 720
+      }
+    })
+      .then(stream => {
+        video.current.srcObject = stream;
+        video.current.play();
+
+        // const canvas = canvas.current;
+        const ctx = canvas.current.getContext("2d");
+        const barcode = new BarcodeDetector({
+          formats: ["qr_code", "ean_13"]
+        });
+        //  formats: ['aztec', 'code_128', 'code_93', 'codebar', 'data_matrix', 'ean_13', 'ean_8', 'itf', 'pdf417', 'qr_code', 'upc_a', 'upc_e']
+        setInterval(() => {
+          canvas.current.width = video.current.videoWidth;
+          canvas.current.height = video.current.videoHeight;
+          ctx.drawImage(
+            video.current,
+            0,
+            0,
+            video.current.videoWidth,
+            video.current.videoHeight
+          );
+          // Burası da okuması aşaması
+          barcode
+            .detect(canvas.current)
+            .then(([data]) => console.log(data))
+            .catch(err => console.log(err))
+        }, 100)
+      })
+      .catch(err => console.log(err))
+  }
+
+
+  // useEffect(() => {
+
+  // }, [])
+
+  return (
+    <>
+      🍀 App.jsx
+      <button onClick={openCam}>Kamerayı Aç</button>
+      <div>
+        <video ref={video} autoPlay muted hidden />
+        <canvas ref={canvas} />
+      </div>
+    </>
+  );
+}
+
+export default App
