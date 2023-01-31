cef1d1e7e76272489d8f4b1871f008d9cedf820b
sebahattin <sebkucuk05@gmail.com>
2023-02-01 00:17:05 +0300

    beforeNewBarcodeDetector

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,7 @@ function App() {
 
   const openCam = () => {
     navigator.mediaDevices.getUserMedia({
-      video: {
-        width: 1280,
-        height: 720
-      }
+      video: { width: 1280, height: 720 }
     })
       .then(stream => {
         video.current.srcObject = stream;
@@ -20,30 +17,25 @@ function App() {
 
         // const canvas = canvas.current;
         const ctx = canvas.current.getContext("2d");
-        const barcode = new BarcodeDetector({
-          formats: ["qr_code", "ean_13"]
-        });
-        //  formats: ['aztec', 'code_128', 'code_93', 'codebar', 'data_matrix', 'ean_13', 'ean_8', 'itf', 'pdf417', 'qr_code', 'upc_a', 'upc_e']
+        // const barcode = new BarcodeDetector({
+        //   formats: ["qr_code", "ean_13"]
+        // });
+        // const barcode = barcodeDetector = new BarcodeDetector({ formats: ['code_39', "qr_code", "ean_13"] });
+
         setInterval(() => {
           canvas.current.width = video.current.videoWidth;
           canvas.current.height = video.current.videoHeight;
-          ctx.drawImage(
-            video.current,
-            0,
-            0,
-            video.current.videoWidth,
-            video.current.videoHeight
-          );
-          // Burası da okuması aşaması
-          barcode
-            .detect(canvas.current)
-            .then(([data]) => console.log(data))
-            .catch(err => console.log(err))
+          ctx.drawImage(video.current, 0, 0, video.current.videoWidth, video.current.videoHeight);
+          // 🔻 Burası da okuması aşaması
+          // barcode
+          //   .detect(canvas.current)
+          //   .then(([data]) => console.log(data))
+          //   .catch(err => console.log(err))
         }, 100)
       })
       .catch(err => console.log(err))
   }
-
+  //  formats: ['aztec', 'code_128', 'code_39', 'code_93','codebar', 'data_matrix', 'ean_13', 'ean_8', 'itf', 'pdf417', 'qr_code', 'upc_a', 'upc_e']
 
   // useEffect(() => {
 
